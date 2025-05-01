a2fa2ff854e5f6547e2c85bcca9bddf7d85faa13
gtmans <96861311+gtmans@users.noreply.github.com>
2025-05-01 16:29:47 +0200

    Update BasicOTAMDNS.ino

--- examples/BasicOTAMDNS.ino
+++ examples/BasicOTAMDNS.ino
@@ -1,11 +1,10 @@
 #include <OTAMDNS.h>               // Inclusie van je herbruikbare OTA/mDNS-bibliotheek
 #include "gewoon_secrets.h"        // Bevat: const char* ssid; const char* password;
-
-//#define SUBADD 123               // Optioneel: zet statisch IP adres op basis 192.168.2.*
-                                   // verander zonodig in OTADNS.cpp IPAddress local_IP(192,168,2,subadd);
-#define OTAVERLEN 5                // Lengte van versiestukje aan einde van IDE bestandsnaam, bv. "Filenaam_V01_0.ino"=5 (lengte V01_0)
-#define OTAPASS   ""               // Optioneel: OTA wachtwoord laat leeg indien nvt.
-#define OTAPREFIX "ESP-OTA000-TST" // Prefix van hostnaam zichtbaar in IDE en mDNS
+#define SUBADD    256              // zet statisch IP adres op basis 192.168.2.* Indien 256 dynamisch van DHCP
+                                   // verander zonodig IP basis in OTADNS.cpp IPAddress local_IP(192,168,2,subadd);
+#define OTAVERLEN 5                // Lengte van versiestukje aan einde van IDE bestandsnaam, bv. "Filenaam_V01_0.ino"=5 (lengte van V01_0)
+#define OTAPASS   ""               // Optioneel: OTA wachtwoord laat leeg indien nvt. Als Arduino IDE vraagt wachtwoord vul letterlijk in ""
+#define OTAPREFIX "D1-OTAMDNSTEST" // Prefix van hostnaam zichtbaar in IDE en mDNS
 
 void setup() {
   Serial.begin(115200);
@@ -13,11 +12,11 @@ void setup() {
   Serial.println("\nStart setup");
 
   // Initialiseer WiFi, mDNS en OTA met één regel
-  OTAMDNS_begin(ssid, password, OTAPREFIX, OTAVERLEN, OTAPASS, SUBADD,__FILE__);
+  OTAMDNS_begin(ssid, password, OTAPREFIX, OTAVERLEN, OTAPASS, SUBADD, __FILE__);
 
   Serial.println("Setup klaar");
 }
 
 void loop() {
-  OTAMDNS_loop();  // Houd OTA service draaiende
+  OTAMDNS_loop();  // Houd OTA service draaiende 
 }
